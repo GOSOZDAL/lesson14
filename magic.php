a9c6f9981f7eee0546b5869021b8bebc3cbb12f3
Munara Mamaeva <munaramamaeva@gmail.com>
2015-11-10 21:04:27 +0600

    initial comit

---
+++ magic.php
@@ -0,0 +1,17 @@
+<?php
+trait Cartrait{
+    public function getWheels(){
+        return $this->Wheels;
+
+    }
+
+
+
+}
+else Car{
+    use Cartrait;
+    public $wheels=4;
+
+}
+$car =new Car();
+echo $car->getWheels();
